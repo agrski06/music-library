0f4cc4feac865dfec89f67dd0ca613bafa4e67ed
adrian <agrski06@gmail.com>
2023-06-15 14:27:51 +0200

    Add "get song by id"

--- backend/src/main/java/agrski/musiclib/controllers/SongController.java
+++ backend/src/main/java/agrski/musiclib/controllers/SongController.java
@@ -5,6 +5,7 @@ import agrski.musiclib.services.SongService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class SongController {
     @GetMapping("")
     public ResponseEntity<List<Song>> getAll() {
         return ResponseEntity.ok(songService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Song> getSongById(@PathVariable Long id) {
+        return ResponseEntity.ok(songService.getById(id));
     }
 
 }
--- backend/src/main/java/agrski/musiclib/services/SongService.java
+++ backend/src/main/java/agrski/musiclib/services/SongService.java
@@ -3,7 +3,9 @@ package agrski.musiclib.services;
 import agrski.musiclib.entities.Song;
 import agrski.musiclib.repositories.SongRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,5 +17,10 @@ public class SongService {
 
     public List<Song> getAll() {
         return songRepository.findAll();
+    }
+
+    public Song getById(Long id) {
+        return songRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Song not found"));
     }
 }
