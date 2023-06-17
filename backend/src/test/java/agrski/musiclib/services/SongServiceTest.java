eb81170458c9270e9467ed2e9e55f46dbf540cc3
adrian <agrski06@gmail.com>
2023-06-17 17:58:45 +0200

    Allow all cors origins

--- backend/src/test/java/agrski/musiclib/services/SongServiceTest.java
+++ backend/src/test/java/agrski/musiclib/services/SongServiceTest.java
@@ -9,14 +9,12 @@ import agrski.musiclib.entities.Song;
 import agrski.musiclib.repositories.AlbumRepository;
 import agrski.musiclib.repositories.ArtistRepository;
 import agrski.musiclib.repositories.SongRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
